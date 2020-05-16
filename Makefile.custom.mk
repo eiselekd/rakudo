905a39e9eea19c90b177b4bc8fbb5dfc1df9bcb9
Konrad Eisele <eiselekd@gmail.com>
2020-05-16 18:10:26 +0200

    all

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,2 +1,7 @@
 all:
 	perl Configure.pl --prefix=/home/eiselekd/bin-moarvm --gen-moar --gen-nqp --backends=moar --moar-option='--debug' --force-rebuild --github-user=eiselekd
+	make
+	make install
+	rm -rf zef
+	git clone https://github.com/ugexe/zef.git
+	cd zef; /home/eiselekd/bin-moarvm/bin/raku -I. bin/zef install .
