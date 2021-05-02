83ca3bb3f53935ee7a21d7805a6b42be9b8b05b4
Konrad Eisele <eiselekd@gmail.com>
2021-05-02 17:15:17 +0200

    all

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -5,5 +5,5 @@ all:
 	rm -rf zef
 	git clone https://github.com/ugexe/zef.git
 	cd zef; /home/eiselekd/bin-moarvm/bin/raku -I. bin/zef install .
-	/home/eiselekd/bin-moarvm/share/perl6/site/bin/zef -v install git://github.com/eiselekd/p6-moarvm-remote.git
-	/home/eiselekd/bin-moarvm/share/perl6/site/bin/zef -v install git://github.com/eiselekd/p6-app-moarvm-debug.git
+	/home/eiselekd/bin-moarvm/share/perl6/site/bin/zef -v install git://github.com/eiselekd/p6-moarvm-remote.git --force-test
+	/home/eiselekd/bin-moarvm/share/perl6/site/bin/zef -v install git://github.com/eiselekd/p6-app-moarvm-debug.git --force-test
